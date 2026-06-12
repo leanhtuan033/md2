3edd4a24956dfd92c9ed6695c0e4e2fa5db29493
leanhtuan033 <anhtuanlw033@gmail.com>
2026-06-12 19:02:26 +0700

    mm

---
+++ src/Loop_Array/SoNguyenTo.java
@@ -0,0 +1,31 @@
+package Loop_Array;
+
+
+public class SoNguyenTo {
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 2;
+
+        while (count < 20) {
+            if (isPrime(number)){
+                System.out.println(number+" ");
+                count++;
+            }
+            number++;
+        }
+    }
+    public static boolean isPrime(int n) {
+        if ( n <2 )
+            return false;
+        for (int i=2; i<= Math.sqrt(n);i++){
+            if (n%i==0)
+                return false;
+        }
+        return true;
+    }
+}
+
+
+
+
+
